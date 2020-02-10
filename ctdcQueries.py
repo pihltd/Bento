40243d8ad4176b412178f66cec179a15d269881c
Todd Pihl <todd.pihl@gmail.com>
2020-02-10 16:31:46 -0500

    Patient mutations

--- ctdcQueries.py
+++ ctdcQueries.py
@@ -21,3 +21,54 @@ def init():
     }
   }
 }"""
+    global diseases
+    diseases = """
+        {
+          case{
+            case_id
+            ctep_category
+          }
+        }
+    """
+    global patients
+    patients = """
+    {
+    case{
+      ethnicity
+      race
+      gender
+      prior_drugs
+      }
+    }
+    """
+
+    global mutant_patients
+    mutant_patients = """
+        {
+            case{
+                case_id
+                specimens{
+                    assignment_reports{
+                        variant_report{
+                            gene_fusion_variants{
+                                gene1
+                                gene2
+                            }
+                            delins_variants{
+                                gene
+                            }
+                            copy_number_variants{
+                                gene
+                            }
+                            snv_variants{
+                                gene
+                            }
+                            indel_variants{
+                                gene
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    """
